c4fd7d9285e21652865b07c85c041c4fd62e9cad
NoisyBass <laura.dcs4@gmail.com>
2018-04-13 23:30:21 +0100

    Adding acceleration and deceleration to player's movement

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,26 +1,31 @@
 extends KinematicBody2D
 
 
-export var acceleration = 25
-export var max_speed = 100
+export var acceleration = 500
+export var deceleration = 300
+export var max_speed = 300
 enum State {IDLE, MOVE, DASH}
 var state
 var direction = Vector2()
+var last_direction = Vector2()
+var speed = 0
 
 func _ready():
 	self.change_state(IDLE)
 
 func _process(delta):
-	var new_direction = Vector2()
-	new_direction.x = float(Input.is_action_pressed("move_right")) - float(Input.is_action_pressed("move_left"))
-	new_direction.y = float(Input.is_action_pressed("move_down")) - float(Input.is_action_pressed("move_up"))
-	if self.state == IDLE and new_direction:
+	if self.direction:
+		self.last_direction = direction
+		
+	self.direction.x = float(Input.is_action_pressed("move_right")) - float(Input.is_action_pressed("move_left"))
+	self.direction.y = float(Input.is_action_pressed("move_down")) - float(Input.is_action_pressed("move_up"))
+	
+	if self.state == IDLE and self.direction:
 		self.change_state(MOVE)
-		self.move_and_collide(new_direction * speed * delta)
-	elif self.state == MOVE:
-		if not new_direction:
-			self.change_state(IDLE)
-		self.move_and_collide(new_direction * speed * delta)
+	elif self.state == MOVE and not self.direction:
+		self.change_state(IDLE)
+			
+	self.move(delta)
 
 func change_state(new_state):
 	match new_state:
@@ -30,5 +35,12 @@ func change_state(new_state):
 			print("Moving")
 	self.state = new_state
 
-func move():
-	pass
+func move(delta):
+	if self.direction:
+		self.speed += self.acceleration * delta
+	else:
+		self.speed -= self.deceleration * delta
+	self.speed = clamp(self.speed, 0, self.max_speed)
+		
+	var motion = self.last_direction.normalized() * self.speed * delta
+	move_and_collide(motion)
