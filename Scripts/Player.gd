75d61290973be5bdb68eaf2e530a88d893ea31f9
Alvarber <alvaro.garcia95@hotmail.com>
2018-04-14 19:56:23 +0100

    Dash can now be interrupted by bounce

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 
 export var player_idx = 0
 export var acceleration = 500
-export var deceleration = 400
+export var deceleration = 800
 export var max_speed = 300
 export var dash_max_speed = 800
 export var dash_duration = 0.5
@@ -20,17 +20,26 @@ var bounce_duration = 0.3
 
 func _ready():
 	$Tween.TweenProcessMode = $Tween.TWEEN_PROCESS_PHYSICS
+	self.execute_state(IDLE, 0)
 
 func _physics_process(delta):
 	# State-independent Input parsing
-	self.direction.x = (
-		float(Input.is_action_pressed("player_%d_move_right" % self.player_idx)) -
-		float(Input.is_action_pressed("player_%d_move_left" % self.player_idx))
-	)
-	self.direction.y = (
-		float(Input.is_action_pressed("player_%d_move_down" % self.player_idx)) -
-		float(Input.is_action_pressed("player_%d_move_up" % self.player_idx))
-	)
+	if len(Input.get_connected_joypads()) > self.player_idx:
+		self.direction = Vector2(
+			Input.get_joy_axis(self.player_idx, JOY_AXIS_0),
+			Input.get_joy_axis(self.player_idx, JOY_AXIS_1)
+		)
+		if self.direction.length() < 0.2:
+			self.direction = Vector2()
+	else:
+		self.direction.x = (
+			float(Input.is_action_pressed("player_%d_move_right" % self.player_idx)) -
+			float(Input.is_action_pressed("player_%d_move_left" % self.player_idx))
+		)
+		self.direction.y = (
+			float(Input.is_action_pressed("player_%d_move_down" % self.player_idx)) -
+			float(Input.is_action_pressed("player_%d_move_up" % self.player_idx))
+		)
 
 	# State changes
 	var new_state = self.state
@@ -48,6 +57,8 @@ func _physics_process(delta):
 					new_state = DASH
 				elif speed == self.max_speed:
 					new_state = MOVE
+				elif not self.direction:
+					new_state = DECELERATE
 			MOVE:
 				if Input.is_action_just_pressed("player_%d_dash" % self.player_idx):
 					new_state = DASH
@@ -63,7 +74,6 @@ func _physics_process(delta):
 			DASH:
 				if self.done_dashing:
 					new_state = IDLE
-					self.done_dashing = false
 			BOUNCE:
 				if self.done_bouncing:
 					new_state = IDLE
@@ -76,30 +86,36 @@ func _physics_process(delta):
 func execute_state(new_state, delta):
 	match [self.state, new_state]:
 		[_, IDLE]:
-			pass
+			self.modulate = Color("#7d7d7d")
 		[_, ACCELERATE]:
 			self.speed += self.acceleration * delta
 			self.speed = clamp(self.speed, 0, self.max_speed)
 			self.move(self.last_direction * self.speed, delta)
+			self.modulate = Color("#bebebe")
 		[_, MOVE]:
 			self.move(self.last_direction * self.speed, delta)
+			self.modulate = Color("#ffffff")
 		[_, DECELERATE]:
 			self.speed -= self.deceleration * delta
 			self.speed = clamp(self.speed, 0, self.max_speed)
 			self.move(self.last_direction * self.speed, delta)
+			self.modulate = Color("#bebebe")
 		[DASH, DASH]:
-			self.move(self.dash_direction * self.speed, self.delta)
+			self.move(self.dash_direction * self.speed, delta)
 		[_, DASH]:
+			self.done_dashing = false
 			$Tween.interpolate_property(
 				self, "speed", self.speed, self.dash_max_speed, self.dash_duration, $Tween.TRANS_BACK, $Tween.EASE_OUT
 			)
 			self.dash_direction = self.last_direction
 			$Tween.start()
 			self._end_dash()
+			self.modulate = Color("#2b2b2b")
 		[BOUNCE, BOUNCE]:
 			self.move(self.speed * self.bounce_direction, delta)
 		[_, BOUNCE]:
-			print("Boiiing")
+			self.done_bouncing = false
+			$Tween.stop_all()
 			$Tween.interpolate_property(
 				self, "speed", self.speed, 0, self.bounce_duration, $Tween.TRANS_LINEAR, $Tween.EASE_IN_OUT
 			)
@@ -120,5 +136,4 @@ func _end_dash():
 func _end_bounce():
 	yield($Tween, "tween_completed")
 	self.done_bouncing = true
-	print("Done bouncing!")
 	self.bounce_direction = null
