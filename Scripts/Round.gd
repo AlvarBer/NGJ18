1cc4fad61036ef484627876b8e3416ffda0154d3
NoisyBass <laura.dcs4@gmail.com>
2018-04-14 16:03:29 +0100

    Instantiating players and map

--- Scripts/Round.gd
+++ Scripts/Round.gd
@@ -6,7 +6,20 @@ export var maps = [
 	preload("res://Scenes/Map3.tscn")
 ]
 
+export var defender = preload("res://Scenes/Defender.tscn")
+export var attacker = preload("res://Scenes/Attacker.tscn")
+
 func start_round(duration, round_number, defender, attacker):
 	var random_idx = randi() % len(maps)
-	var map = maps[random_idx].instance()
+	var map = self.maps[random_idx].instance()
 	self.add_child(map)
+	
+	var d = self.defender.instance()
+	d.player_idx = defender
+	d.position = map.get_node("PosPlayer" + str(defender)).position;
+	self.add_child(d)
+	
+	var a = self.attacker.instance()
+	a.player_idx = attacker
+	a.position = map.get_node("PosPlayer" + str(attacker)).position;
+	self.add_child(a)
