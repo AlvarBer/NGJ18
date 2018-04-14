ecc4dc0d95a6e7dc6f473363bd9a655038fcbeba
NoisyBass <laura.dcs4@gmail.com>
2018-04-14 15:40:05 +0100

    Working on game flow

--- Scripts/Match.gd
+++ Scripts/Match.gd
@@ -6,17 +6,18 @@ var players_points = [0, 0]
 var rounds = 0
 
 func _ready():
+	randomize()
 	self.start_round()
 	
 func start_round():
 	$Round.start_round(5, rounds, 0, 1)
 	
 func finish_round(winner):
-	players_points[winner] += 1
-	rounds += 1
+	self.players_points[winner] += 1
+	self.rounds += 1
 	
-	if players_points[winner] > max_rounds / 2:
-		finish_match(winner)
+	if self.players_points[winner] > self.max_rounds / 2:
+		self.finish_match(winner)
 		
 func finish_match(winner):
-	print ("Winner: Player " + str(winner +1))
+	print("Winner: Player " + str(winner +1))
--- Scripts/Round.gd
+++ Scripts/Round.gd
@@ -1,4 +1,12 @@
 extends Node
 
-func start_match(duration, round_number, defends, attacks):
-	pass
+export var maps = [ 
+	preload("res://Scenes/Map1.tscn"), 
+	preload("res://Scenes/Map2.tscn"), 
+	preload("res://Scenes/Map3.tscn")
+]
+
+func start_round(duration, round_number, defender, attacker):
+	var random_idx = randi() % len(maps)
+	var map = maps[random_idx].instance()
+	self.add_child(map)
