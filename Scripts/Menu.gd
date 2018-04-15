7e4c912c96efcc644fa64d5e03e0cce96a002096
NoisyBass <laura.dcs4@gmail.com>
2018-04-15 11:08:41 +0100

    Menu screen finished

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,9 +1,8 @@
 extends Node
 
 func _ready():
-	$MarginContainer/VBoxContainer/Start.grab_focus()
+	$Start.grab_focus()
 
 
 func _on_Start_pressed():
-	print ("Pressed")
 	get_node("/root/Global").goto_scene("res://Scenes/Match.tscn")
